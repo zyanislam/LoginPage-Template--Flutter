4a0f447e7748c4eca183101ed247dced49606169
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-17 17:39:40 +0600

    Update: Background added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 import "modules/login_page.dart";
 
@@ -13,6 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
--- lib/modules/login_page.dart
+++ lib/modules/login_page.dart
@@ -2,19 +2,23 @@
 
 import "package:flutter/material.dart";
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Login Page")),
+      // appBar: AppBar(
+      //   title: Center(child: Text("Login Page")),
+      // ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/1.jpg'), fit: BoxFit.cover)),
+          )
+        ],
       ),
     );
   }
