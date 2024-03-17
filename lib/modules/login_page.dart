0561abf496d7ada2e637ed11754ecbe89f99dafb
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-17 21:05:55 +0600

    Update: Logo and Welcome text added

--- lib/modules/login_page.dart
+++ lib/modules/login_page.dart
@@ -1,33 +1,42 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Center(child: Text("Login Page")),
-      // ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/2.png'), fit: BoxFit.cover)),
+      backgroundColor: Color.fromARGB(252, 16, 16, 20),
+      body: SafeArea(
+        // body: Container(
+        //   // decoration: BoxDecoration(
+        //   //   image: DecorationImage(
+        //   //     image: AssetImage("images/2.png"),
+        //   //     fit: BoxFit.cover,
+        //   //   ),
+        //   // ),
+        //   child: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 200),
+              Image.asset(
+                "images/Logo.png",
+                width: 180,
+              ),
+              SizedBox(height: 100),
+              Text(
+                "Welcome back!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Colors.transparent,
-              Colors.transparent,
-              Color(0xff161d27).withOpacity(0.9),
-              Color(0xff161d27)
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          ),
-        ],
+        ),
       ),
     );
   }
