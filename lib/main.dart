af5ae8f6b5ef53ee224704af7a8625418e9861de
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-18 19:54:40 +0600

    Update: AuthPage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import 'package:login_signup/modules/auth_page.dart';
 import "modules/login_page.dart";
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
     //     overlays: [SystemUiOverlay.bottom]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthPage(),
     );
   }
 }
--- lib/modules/home_page.dart
+++ lib/modules/home_page.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text("Logged In"),
+    );
   }
 }
