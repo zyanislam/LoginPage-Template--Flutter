2265e9d168dbff2564ed13c7c5ff80b475281045
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-18 19:46:12 +0600

    Update: Firebase Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
-
 import "modules/login_page.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
