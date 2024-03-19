7038252787d6702cd86626c28671daaf98e34b8c
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-19 19:36:32 +0600

    Update: Sign Out Function

--- lib/modules/home_page.dart
+++ lib/modules/home_page.dart
@@ -1,13 +1,29 @@
 // ignore_for_file: prefer_const_constructors
 
+import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          "Home",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        )),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
         child: Text("Logged In"),
       ),
