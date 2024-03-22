ff44968fc8a0962ad7b50d87c674761c03b4cac7
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-23 03:01:26 +0600

    Update: NavBar done

--- lib/components/bottomNav.dart
+++ lib/components/bottomNav.dart
@@ -14,14 +14,25 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: GNav(
-        gap: 8,
-        tabs: [
-          GButton(icon: Icons.home, text: 'Home'),
-          GButton(icon: Icons.explore, text: 'Explore'),
-          GButton(icon: Icons.favorite_outline, text: 'Likes'),
-          GButton(icon: Icons.person, text: 'User'),
-        ],
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+            gap: 8,
+            padding: EdgeInsets.all(16),
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Color.fromARGB(255, 154, 135, 212),
+            tabBackgroundColor: Colors.grey.shade800,
+            tabs: [
+              GButton(icon: Icons.home, text: 'Home'),
+              GButton(icon: Icons.explore, text: 'Explore'),
+              GButton(icon: Icons.favorite_outline, text: 'Likes'),
+              GButton(icon: Icons.person, text: 'User'),
+            ],
+          ),
+        ),
       ),
     );
   }
